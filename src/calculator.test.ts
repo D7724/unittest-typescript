75007534541c2bbd31de93b765ec905fb94bde39
D7724 <Ryan_Truttmann@sluz.ch>
2026-03-13 14:32:53 +0100

    Aufgabe 2 Tests manipulieren

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -2,7 +2,7 @@ import { add, subtract } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
-  const a: number = 2;
+  const a: number = 4;
   const b: number = 3;
   const expected: number = 5;
 
@@ -15,7 +15,7 @@ test("add three to two is five", () => {
 
 test("subtract four from six is two", () => {
   // Arrange
-  const a: number = 6;
+  const a: number = 7;
   const b: number = 4;
   const expected: number = 2;
 
